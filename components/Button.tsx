2a6afc42fd8f548fd359e0e4961e0b7b70bc3134
m.bebars <66588352+mahmoud-bebars@users.noreply.github.com>
2023-10-23 01:04:44 +0300

    updating Home Page Components & Adding Rest of Home Page Components with all styles needed - Finished Project 100%

---
+++ components/Button.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+type ButtonProps = {
+  type: "button" | "submit";
+  title: string;
+  icon?: string;
+  varient: string;
+  full?: boolean;
+};
+
+const Button = ({ type, title, icon, varient, full }: ButtonProps) => {
+  return (
+    <button
+      type={type}
+      className={`flexCenter gap-3 rounded-full border ${varient} ${
+        full && "w-full"
+      }`}
+    >
+      {icon && <Image alt="button-icon" src={icon} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap cursor-pointer">
+        {title}
+      </label>
+    </button>
+  );
+};
+
+export default Button;
